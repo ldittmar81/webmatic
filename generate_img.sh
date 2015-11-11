2f58b282465ce52ee17220060e713fdff10b6957
ldittmar81 <ldittmar.privat@gmail.com>
2015-11-11 23:59:12 +0100

    Installationsdateiersteller angepasst

--- generate_img.sh
+++ generate_img.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 mkdir -p tmp/webmatic
-cp -a webmatic/* tmp/webmatic
+mkdir -p tmp/webmatic/cgi
+mkdir -p tmp/webmatic/debug
+mkdir -p tmp/webmatic/img
+mkdir -p tmp/webmatic/js
+mkdir -p tmp/webmatic/jqplot
+mkdir -p tmp/webmatic/jquery
+mkdir -p tmp/webmatic/themes
+mkdir -p tmp/webmatic/themes/images
+cp webmatic/* tmp/webmatic
+cp webmatic/cgi/* tmp/webmatic/cgi
+cp webmatic/debug/* tmp/webmatic/debug
+cp -a webmatic/img/* tmp/webmatic/img
+cp -a webmatic/jqplot/* tmp/webmatic/jqplot
+cp -a webmatic/jquery/* tmp/webmatic/jquery
+cp webmatic/js/*.min.js tmp/webmatic/js
+cp webmatic/themes/*.min.css tmp/webmatic/themes
+cp -a webmatic/themes/images/* tmp/webmatic/themes/images
+
 mkdir -p tmp/webmatic_user
 cp -a webmatic_user/* tmp/webmatic_user
 cp update_script tmp/
