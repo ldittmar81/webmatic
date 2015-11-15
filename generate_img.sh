7d462a0041395bd9eea3f63ed6d70428e9a45c53
Jens Maus <mail@jens-maus.de>
2015-11-15 23:37:02 +0100

    added CCU specific 'update_addon' which allows to add a global menu entry to the WebUI. Also restructured generate_img.sh script to be more general. Furthermore, renamed the rc.d init script to use the same name like the addon.

--- generate_img.sh
+++ generate_img.sh
@@ -1,29 +1,15 @@
 #!/bin/sh
-mkdir -p tmp/webmatic
-mkdir -p tmp/webmatic/cgi
-mkdir -p tmp/webmatic/debug
-mkdir -p tmp/webmatic/img
-mkdir -p tmp/webmatic/js
-mkdir -p tmp/webmatic/libs
-mkdir -p tmp/webmatic/themes
-mkdir -p tmp/webmatic/themes/images
-mkdir -p tmp/webmatic/themes/fonts
-cp webmatic/* tmp/webmatic
-cp webmatic/cgi/* tmp/webmatic/cgi
-cp webmatic/debug/* tmp/webmatic/debug
-cp -a webmatic/img/* tmp/webmatic/img
-cp -a webmatic/libs/* tmp/webmatic/libs
-cp webmatic/js/*.min.js tmp/webmatic/js
-cp webmatic/themes/*.min.css tmp/webmatic/themes
-cp -a webmatic/themes/images/* tmp/webmatic/themes/images
-cp -a webmatic/themes/fonts/* tmp/webmatic/themes/fonts
-
-mkdir -p tmp/webmatic_user
-cp -a webmatic_user/* tmp/webmatic_user
-cp update_script tmp/
-cp webmatic-dlg tmp/
-cp VERSION tmp/
+mkdir -p tmp
+cp -a webmatic tmp/
+cp -a webmatic_user tmp/
+cp -a ccu1 tmp/
+cp -a ccu2 tmp/
+cp -a ccurm tmp/
+cp -a rc.d tmp/
+cp -a update_script tmp/
+cp -a VERSION tmp/
 cd tmp
-tar -czvf ../webmatic-$(cat ../VERSION).tar.gz *
+
+tar --owner=root --group=root -czvf ../webmatic-$(cat ../VERSION).tar.gz *
 cd ..
 rm -rf tmp
